2061e719097754f3f637e3381236621f8fe38b94
ad1b003 <76702138+ad1b003@users.noreply.github.com>
2023-07-11 12:45:34 +0600

    Update push.js
    Make the script tags run
    Smooth transition

--- push.js
+++ push.js
@@ -2,27 +2,37 @@ async function pushContent(file, target = 'body') {
   const fetchedObject = await fetch(file);
   const text = await fetchedObject.text();
   const content = await extract(text);
-  const tElement = document.querySelector(target);
-  tElement.innerHTML = content;
-  tElement.classList.remove('slide-in');
-  await makeTheScriptsRun(tElement);
-  tElement.classList.add('slide-in');
+  const tElement = document.querySelector(target); // 🎯 element
+  tElement.classList.remove('slide-in'); // remove for transition
+  tElement.innerHTML = content; // swapping content
+  await makeTheScriptsRun(tElement);;
+  tElement.classList.add('slide-in'); // add new transition
 }
 
+// extract what is in between body tag
 async function extract(strVal) {
   const pattern = /<body[^>]*>((.|[\n\r])*)<\/body>/im;
   const arrayOfMatched = await pattern.exec(strVal);
-  return arrayOfMatched[1]; // innerHTML
+  return arrayOfMatched[1]; // HTML inside <body></body>
 }
 
+/*
+    * by injecting HTML using innerHTML, code inside the script tags never get executed
+    * to execute the code, we are -
+      * creating a new script tag with its attributes and inner text (code)
+      * then replacing it with the original one
+      * repeating it for each one of the script tags
+ */
+
+// replacing
 function replaceScript(element) {
   element.parentElement.replaceChild(cloneScript(element), element);
 }
 
+// cloning / creating a script tag
 function cloneScript(element) {
   const script = document.createElement("script");
   script.innerText = element.textContent;
-
   let i = -1,
     attrs = element.attributes,
     attr;
@@ -34,7 +44,7 @@ function cloneScript(element) {
 
 async function makeTheScriptsRun(parent = document.body) {
   Array.from(document.getElementsByTagName('script')).forEach(tag => {
-    if (tag.parentElement === parent) {
+    if (tag.parentElement === parent) { // only script tags inside the fetched HTML
       replaceScript(tag);
     }
   })
@@ -43,7 +53,7 @@ async function makeTheScriptsRun(parent = document.body) {
 // getting all elements which have push attribute
 document.querySelectorAll('[push]').forEach(x => {
   const href = x.getAttribute('push'); // link
-  const target = x.getAttribute('push-target'); // wher will the content go
+  const target = x.getAttribute('push-target'); // where will the content go
   // adding click event
   x.addEventListener('click', function() {
     if (href !== '') {
